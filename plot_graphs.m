481ed541791fe259aa4fda75d504566200aa4e3c
Sarra ELGHALI <sarra.elghali@gmail.com>
2018-04-05 15:10:29 +0200

    add spaces to plot_graphs function

--- plot_graphs.m
+++ plot_graphs.m
@@ -1,14 +1,16 @@
+% plot planned trajectory , executed trajectory and the difference 
+
 function plot_graphs(handles,real_time,theor_time,diff_trajectory,thero_trajectory,realtrajectory,i,ymin,ymax)
-axes(handles.axestheorique);
-plot(theor_time,thero_trajectory(i,:),'b',real_time,realtrajectory(i,:),'--r');
-xlabel('time');
-ylabel('trajectory');
-legend('theorical','real');
-limits=[ymin,ymax];
-ylim(limits);
-if (get(handles.checkbox1, 'Value')==1)
-        diff_trajectory=abs(diff_trajectory);
-    end
-        
-axes(handles.axesdiff);
-plot(theor_time,diff_trajectory(i,:),'g');
+    axes(handles.axestheorique);
+    plot(theor_time,thero_trajectory(i,:),'b',real_time,realtrajectory(i,:),'--r');
+    xlabel('time');
+    ylabel('trajectory');
+    legend('theorical','real');
+    limits=[ymin,ymax];
+    ylim(limits);
+    if (get(handles.checkbox1, 'Value')==1)
+            diff_trajectory=abs(diff_trajectory);
+        end
+
+    axes(handles.axesdiff);
+    plot(theor_time,diff_trajectory(i,:),'g');
