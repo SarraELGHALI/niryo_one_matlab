2e543f8f37dc747b1685476144b8738b81e5b705
Sarra ELGHALI <sarra.elghali@gmail.com>
2018-04-05 15:00:24 +0200

    add open_folder function to import_trajectorym

--- import_trajectory.m
+++ import_trajectory.m
@@ -4,3 +4,14 @@ function [thero_trajectory,real_trajectory,diff_trajectory]=import_trajectory()
     thero_trajectory=Trajectories(1:7,:);
     real_trajectory=Trajectories(8:14,:);
     diff_trajectory=Trajectories(15:21,:);
+    
+    
+function [fullFileName]=open_folder()
+    startingFolder = userpath % Or "pwd" or wherever you want.
+    defaultFileName = fullfile(startingFolder, '*.xls');
+    [baseFileName, folder] = uigetfile(defaultFileName, 'Specify a file');
+    if baseFileName == 0
+        % User clicked the Cancel button.
+        return;
+    end
+    fullFileName = fullfile(folder, baseFileName);
