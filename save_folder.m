b5aef9a6896945ff0531344e7961c119ee765db4
Sarra ELGHALI <sarra.elghali@gmail.com>
2018-04-05 14:55:19 +0200

    remove save_folder.m and add the funcion save_folder in export_trajectory.m file

--- save_folder.m
+++ save_folder.m
@@ -1,9 +1,1 @@
- function [fullFileName]=save_folder()
-startingFolder = userpath % Or "pwd" or wherever you want.
-defaultFileName = fullfile(startingFolder, '*.txt');
-[baseFileName, folder] = uiputfile(defaultFileName, 'Specify a file');
-if baseFileName == 0
-	% User clicked the Cancel button.
-	return;
-end
-fullFileName = fullfile(folder, baseFileName);
+ 
