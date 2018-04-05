23b3a778e43d56d92600d807e6063cb4cb2d50b6
Sarra ELGHALI <sarra.elghali@gmail.com>
2018-04-05 14:59:23 +0200

    remove function open_folder and save_folder

--- open_folder.m
+++
@@ -1,9 +0,0 @@
-function [fullFileName]=open_folder()
-    startingFolder = userpath % Or "pwd" or wherever you want.
-    defaultFileName = fullfile(startingFolder, '*.xls');
-    [baseFileName, folder] = uigetfile(defaultFileName, 'Specify a file');
-    if baseFileName == 0
-        % User clicked the Cancel button.
-        return;
-    end
-    fullFileName = fullfile(folder, baseFileName);
