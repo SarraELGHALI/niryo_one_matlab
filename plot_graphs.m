91e61ec1801f56980331d9e807b01a67fc5e3799
Sarra ELGHALI <sarra.elghali@gmail.com>
2018-04-05 10:59:00 +0200

    change input variables names

--- plot_graphs.m
+++ plot_graphs.m
@@ -1,11 +1,10 @@
-function plot_graphs(handles,time,x,diff,joint_position,JointStatePosition,i,ymin,ymax)
-    axes(handles.axestheorique);
-  
-    plot(x,joint_position(i,:),'b',time,JointStatePosition(i,:),'--r');
-    xlabel('time')
-    ylabel('trajectory')
-    legend('theorical','real')
-    limits=[ymin,ymax]
-    ylim(limits);
-    axes(handles.axesdiff);
-    plot(x,diff(i,:),'g');
+function plot_graphs(handles,real_time,theor_time,diff_trajectory,thero_trajectory,realtrajectory,i,ymin,ymax)
+axes(handles.axestheorique);
+plot(theor_time,thero_trajectory(i,:),'b',real_time,realtrajectory(i,:),'--r');
+xlabel('time');
+ylabel('trajectory');
+legend('theorical','real');
+limits=[ymin,ymax];
+ylim(limits);
+axes(handles.axesdiff);
+plot(theor_time,diff_trajectory(i,:),'g');
