00d7fd263aa125d2f5341c479412a5ed96fb693d
Sarra ELGHALI <sarra.elghali@gmail.com>
2018-04-09 15:47:28 +0200

    add try catch statement to catch error while ploting

--- plot_graphs.m
+++ plot_graphs.m
@@ -19,27 +19,27 @@
 % plot planned trajectory , executed trajectory and the difference 
 
 function plot_graphs(handles,real_time,theor_time,diff_trajectory,thero_trajectory,realtrajectory,i,ymin,ymax)
-    axes(handles.axestheorique);
+   
     try
+        axes(handles.axestheorique);
         plot(theor_time,thero_trajectory(i,:),'b',real_time,realtrajectory(i,:),'--r');
+        xlabel('time');
+        ylabel('trajectory');
+        legend('theorical','real');
+        limits=[ymin,ymax];
+        ylim(limits);
+        grid on 
+        if (get(handles.checkbox1, 'Value')==1)
+            diff_trajectory=abs(diff_trajectory);
+        end
+
+        axes(handles.axesdiff);
+        diff_trajectory(i,:)=round(diff_trajectory(i,:),2);
+        plot(theor_time,diff_trajectory(i,:),'g');
+        line(get(gca,'XLim'), [0 0], 'Color', 'k', 'LineStyle', '--');
+         grid on 
+        legend('difference');
     catch e
         disp("error try again ") 
         return; 
     end
-    xlabel('time');
-    ylabel('trajectory');
-    legend('theorical','real');
-    limits=[ymin,ymax];
-    ylim(limits);
-    grid on 
-    if (get(handles.checkbox1, 'Value')==1)
-        diff_trajectory=abs(diff_trajectory);
-    end
-
-    axes(handles.axesdiff);
-    diff_trajectory(i,:)=round(diff_trajectory(i,:),2);
-    plot(theor_time,diff_trajectory(i,:),'g');
-    line(get(gca,'XLim'), [0 0], 'Color', 'k', 'LineStyle', '--');
-     grid on 
-    legend('difference');
-   
